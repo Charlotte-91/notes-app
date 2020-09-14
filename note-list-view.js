7385ccca3c1cf6a87fe1ab9a23b4ad5f8e696b0c
Iliyan-Y <maximizebg@gmail.com>
2020-09-14 18:18:32 +0100

    All files saved

--- note-list-view.js
+++ note-list-view.js
@@ -5,12 +5,14 @@ class NoteListView {
 
   htmlList() {
     let firstNote;
+
     if (this.noteListModel.displayNotes().length > 0) {
       let notes = this.noteListModel.displayNotes();
       firstNote = notes[0].text;
     } else {
       firstNote = "";
     }
+
     return `<ul><li><div>${firstNote}</div></li></ul>`;
   }
 }
